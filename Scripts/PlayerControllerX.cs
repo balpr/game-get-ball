171d33bdd14a6b6ab430cf43d411428e3ae344a6
Achmad Iqbal Priranda <79825994+balpr@users.noreply.github.com>
2022-04-11 16:59:07 +0700

    Add files via upload

---
+++ Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    [SerializeField]
+    private float cooldownTimer= 2.0f;
+    private bool isCooldown= false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !isCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            isCooldown= true;
+            StartCoroutine("ResetCooldown");
+        }
+    }
+
+    IEnumerator ResetCooldown()
+    {
+        yield return new WaitForSeconds(cooldownTimer);
+        isCooldown= false;
+    }
+}
---
+++ Scripts/SpawnManagerX.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    // private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+    private bool canSpawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    void Update()
+    {
+        if(canSpawn)
+        {
+            SpawnRandomBall();
+            canSpawn = false;
+            StartCoroutine("SpawnDelay");
+        }
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        //Generate random ball index and random color ball
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+    IEnumerator SpawnDelay()
+    {
+        spawnInterval = Random.Range(3.0f,5.0f);
+        yield return new WaitForSeconds(spawnInterval);
+        canSpawn = true;
+    }
+
+}
